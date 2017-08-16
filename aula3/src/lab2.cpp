62ec0be339905ff6399aa5fe4eab6d4d7f0c1047
Luiz Eduardo Sol <luizedusol@gmail.com>
2017-08-16 16:21:42 -0300

    Fim da terceira aula

--- aula3/src/lab2.cpp
+++ aula3/src/lab2.cpp
@@ -23,110 +23,46 @@ void sistema(){
 
   string option = "";
 
-  while(option.compare("X") != 0){
+  while(true){
+    cout << endl << "-------------------------" << endl;
     cout << novaEmpresa.getNome() << endl;
     cout << "-------------------------" << endl << endl;
 
     cout << "[C] Cadastrar Funcionario" << endl;
     cout << "[D] Demitir Funcionario" << endl;
+    cout << "[T] Demitir Todos Funcionarios" << endl;
     cout << "[M] Mostrar Funcionarios" << endl;
     cout << "[X] Sair" << endl;
-    getline(cin, nomeEmpresa);
+    getline(cin, option);
+    cout << "-------------------------" << endl << endl;
 
+    switch(option.at(0)){
+    case 'C':
+    case 'c':
+      novaEmpresa.contratarFuncionarioIterativo();
+      break;
+    case 'D':
+    case 'd':
+      novaEmpresa.demitirFuncionarioIterativo();
+      break;
+    case 'T':
+    case 't':
+      novaEmpresa.demitirTodosFuncionarios();
+      break;
+    case 'M':
+    case 'm':
+      novaEmpresa.obterDadosFuncionarios();
+      break;
+    case 'X':
+    case 'x':
+      return;
+    default:
+      break;
+    }
   }
-
 }
 
 int main(int argc, char * argv[]){
-
-  /* A. Cria dinamicamente um objeto do tipo Empresa; seu nome deve ser
-   * solicitado ao usuário. Após a criação, não existe nenhum funcionário
-   * contratado na empresa.
-   */
-  string nomeEmpresa;
-  cout << "Forneca o nome da empresa:" << endl;
-  getline(cin, nomeEmpresa);
-  Empresa novaEmpresa(nomeEmpresa, 5);
-
-
-  /* B. Apresenta na tela os dados dos funcionários contratados.
-   *
-   */
-  cout << "Apresentando os dados sobre os funcionarios da empresa:" << endl;
-  novaEmpresa.obterDadosFuncionarios();
-
-
-  /* C. Contrata 2 funcionários, com dados fornecidos pelo teclado e escolhidos
-   * por vocês.
-   */
-  novaEmpresa.contratarFuncionarioIterativo();
-
-  novaEmpresa.contratarFuncionarioIterativo();
-
-  /* D. Apresenta na tela os dados dos funcionários contratados.
-   */
-  cout << "Apresentando os dados sobre os funcionarios da empresa:" << endl;
-  novaEmpresa.obterDadosFuncionarios();
-
-  /* E. Demite um funcionário, fornecendo um valor inexistente de idFuncional.
-   */
-  novaEmpresa.demitirFuncionarioIterativo();
-
-  /* F. Demite o 1º funcionário contratado, fornecendo sua identificação
-   * funcional pelo teclado.
-   */
-  novaEmpresa.demitirFuncionarioIterativo();
-
-  /* G. Apresenta na tela os dados dos funcionários contratados.
-   */
-  cout << "Apresentando os dados sobre os funcionarios da empresa:" << endl;
-  novaEmpresa.obterDadosFuncionarios();
-
-  /* H. Contrata mais 3 funcionários, com dados fornecidos pelo teclado e
-   * escolhidos por vocês.
-   */
-  novaEmpresa.contratarFuncionarioIterativo();
-
-  novaEmpresa.contratarFuncionarioIterativo();
-
-  novaEmpresa.contratarFuncionarioIterativo();
-
-  /* I. Demite o último funcionário contratado, fornecendo sua identificação
-   * funcional pelo teclado.
-   */
-  novaEmpresa.demitirFuncionarioIterativo();
-
-  /* J. Apresenta na tela os dados dos funcionários contratados.
-   */
-  cout << "Apresentando os dados sobre os funcionarios da empresa:" << endl;
-  novaEmpresa.obterDadosFuncionarios();
-
-  /* K. Demite todos os funcionários da empresa.
-   */
-  novaEmpresa.demitirTodosFuncionarios();
-
-  /* L. Apresenta na tela os dados dos funcionários contratados.
-   */
-  cout << "Apresentando os dados sobre os funcionarios da empresa:" << endl;
-  novaEmpresa.obterDadosFuncionarios();
-
-  /* M. Demite mais um funcionário.
-   */
-  novaEmpresa.demitirFuncionarioIterativo();
-
-  /* N. Testar o caso em que a empresa não tem mais vagas para contratação.
-   */
-  novaEmpresa.contratarFuncionario("0123", "Carlos", "Rua dos bobos, #0",
-                                   "Estivador", "Carregar Pedra", "Junior");
-  novaEmpresa.contratarFuncionario("013", "Alberto", "Rua dos bobos, #0",
-                                   "Estivador", "Carregar Pedra", "Junior");
-  novaEmpresa.contratarFuncionario("0144423", "Adriano", "Rua dos bobos, #0",
-                                   "Estivador", "Carregar Pedra", "Junior");
-  novaEmpresa.contratarFuncionario("03", "Pedro", "Rua dos bobos, #0",
-                                   "Estivador", "Carregar Pedra", "Junior");
-
-
-
-  cout << "Bye" << endl;
+  sistema();
   return 0;
 }
