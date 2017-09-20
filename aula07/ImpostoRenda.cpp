9e02429608a481e03569bd9d52feb4e6aaa5ee7f
AugRM <augustormachado@gmail.com>
2017-09-20 16:42:01 -0300

    Versão final aula7

---
+++ aula07/ImpostoRenda.cpp
@@ -0,0 +1,69 @@
+/**
+PCS2478 - Tópicos de Programação
+Empresa.cpp
+
+@author 8586861 - Luiz Eduardo Sol (luizedusol@gmail.com)
+@author 7576829 - Augusto Ruy Machado (augustormachado@gmail.com)
+
+@version 1.0 2017-09-20
+*/
+
+#include "ImpostoRenda.h"
+#include <iostream>
+
+using namespace std;
+
+ImpostoRenda::ImpostoRenda()
+{
+}
+
+ImpostoRenda::~ImpostoRenda()
+{
+}
+
+ImpostoRenda::ImpostoRenda(string nomeArquivo) {
+	this->nomeArquivo = nomeArquivo;
+	arquivo.open(nomeArquivo.c_str(), ios::app);
+}
+
+vector<float> ImpostoRenda::splitDado(string dados)
+{
+	vector<float> result;
+	unsigned int start = 0;
+	unsigned int i = 1;
+
+	while (i < dados.length()) {
+		if (dados.at(i) == '|') {
+			if (start == i) {
+				throw std::invalid_argument("String invalida.");
+			}
+			result.push_back(stof(dados.substr(start, i - start)));
+			start = i + 1;
+		}
+		i++;
+	}
+
+	return result;
+}
+
+float ImpostoRenda::calcularIR(float salario)
+{
+	float ir = 0.0;
+	string linha = "";
+	string nome = "tabdir.dat";
+	arquivo.open(nome.c_str(), ios::in);
+
+	while (arquivo.good()) {
+		getline(arquivo, linha);
+		vector<float> vlinha = splitDado(linha);
+		if (salario >= vlinha[0]) {
+			if (salario <= vlinha[1]) {
+				ir = salario * vlinha[2]/100 - vlinha[3];
+			}
+		}
+	}
+
+	return ir;
+}
+
+
---
+++ aula07/ImpostoRenda.h
@@ -0,0 +1,40 @@
+/**
+PCS2478 - Tópicos de Programação
+Empresa.cpp
+
+@author 8586861 - Luiz Eduardo Sol (luizedusol@gmail.com)
+@author 7576829 - Augusto Ruy Machado (augustormachado@gmail.com)
+
+@version 1.0 2017-09-20
+*/
+
+#pragma once
+
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <fstream>
+
+#pragma once
+
+using namespace std;
+
+class ImpostoRenda {
+
+private:
+	fstream arquivo;
+	string nomeArquivo;
+
+public:
+	ImpostoRenda();
+
+	~ImpostoRenda();
+
+	ImpostoRenda(string nomeArquivo);
+
+	vector<float> splitDado(string dados);
+
+	float calcularIR(float salario);
+
+};
