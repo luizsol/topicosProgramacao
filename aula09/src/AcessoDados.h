ec3ae9c2cdca7b2199e0edd9e54d18058b899071
AugRM <augustormachado@gmail.com>
2017-10-11 16:11:41 -0300

    Fim EP7

---
+++ aula09/src/AcessoDados.h
@@ -0,0 +1,35 @@
+#pragma once
+#pragma once
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "globais.h"
+
+using namespace std;
+
+class AcessoDados {
+private:
+	fstream arquivo;
+	string nomeArquivo;
+	vector<string> nomesArqs = {"cadpessoas.dat", "tabsalarial.dat", "tabcs.dat", "tabir.dat" };
+	enum modosAcesso{LEITURA, ESCRITA, INSERCAO};
+
+public:
+
+	AcessoDados();
+
+	~AcessoDados();
+	
+	string lerTudo(Arquivos arq);
+
+	bool conectar(Arquivos arq, modosAcesso modo);
+
+	void desconectar(Arquivos arq);
+
+	string ler(Arquivos arq, string valChave, Campos chave);
+
+	vector<string> splitDado(string dado);
+
+};
