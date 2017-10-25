c206d138905f86bf0f2fa5f3212e752338ef8e03
AugRM <augustormachado@gmail.com>
2017-10-24 22:56:23 -0200

    Aula 11 finalizada
    Com erro de classe possivelmente da minha versão do VS

---
+++ aula11/src/CadastroPessoas.h
@@ -0,0 +1,145 @@
+/**
+    PCS2478 - Tópicos de Programação
+    CadastroPessoas.h
+
+    @author 8586861 - Luiz Eduardo Sol (luizedusol@gmail.com)
+    @author 7576829 - Augusto Ruy Machado (augustormachado@gmail.com)
+    @version 1.0 2017-10-11
+*/
+
+#pragma once
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include "Funcionario.h"
+#include "AcessoDados.h"
+
+using namespace std;
+
+class CadastroPessoas{
+  vector<string> dadosPessoais;
+  AcessoDados acessoDados;// Objeto de acesso ao acesso dados
+
+public:
+  // Construtores da classe CadastroPessoas
+  CadastroPessoas();
+  CadastroPessoas(string cargaInicial);
+
+  // Destrutor da classe empresa
+  ~CadastroPessoas();
+
+  // Setters e Getters
+  vector<string> getDadosPessoais();
+
+  /**
+      Valida uma string de dados de cadastro.
+
+      @param dados (string): a string a ser validada
+      @return true se a string for válida, false caso contrário
+  */
+  bool validarDados(string dados);
+
+  /**
+      Adiciona uma string de dados ao array de dados pessoais.
+
+      @param dados (string): a string a ser adicionada
+      @param sobrescrever (bool): true se a adição deve sobrescrever os dados de
+                                  um usuário com o mesmo idPessoa
+      @return true se a string foi adicionada, false caso contrário
+      @throw caso haja a tentativa de sobrescrever um usuário e o argumento
+             'sobrescrever' seja false (domain_error)
+  */
+  bool adicionarDadosPessoa(string dados, bool sobrescrever=true);
+
+  /**
+      Adiciona uma string contendo várias strings de dados ao array de dados
+      pessoais.
+
+      @param dados (string): a string dos dados a serem adicionados
+      @param sobrescrever (bool): true se a adição deve sobrescrever os dados de
+                                  um usuário com o mesmo idPessoa
+      @return true se todos os dados foram adicionados, false caso contrário
+      @throw caso sejam passados dados inválidos (invalid_argument)
+      @throw caso outro cadastro já possua esse ID Pessoa (domain_error)
+  */
+  bool adicionarDadosPessoas(bool sobrescrever=true);
+
+  /**
+      Retorna uma string obtida pela concatenação de todos os dados no vetor de
+      dados pessoais.
+
+      @return a string resultante da concatenação de todos os dados contidos no
+              vetor de dados pessoais
+  */
+  string lerDadosTodasPessoas();
+
+  /**
+      Retorna uma string com os dados de uma pessoa.
+
+      @param idPessoa (string): o id da pessoa a ser buscada
+      @return a string contendo os dados da pessoa
+      @throw caso os idPessoa não exista (domain_error)
+  */
+  string lerDadosPessoa(string idPessoa);
+
+  /**
+      Atualiza os dados de uma pessoa.
+
+      @param dados (string): os novos dados a serem atualizados
+      @return true se a operação foi bem sucedida, false caso contrario
+      @throw caso sejam passados dados inválidos (invalid_argument)
+      @throw caso os idPessoa não exista (domain_error)
+  */
+  bool atualizarDadosPessoa(string dados);
+
+  /**
+      Retorna um vector de strings contendo os diferentes campos de uma linha de
+      dados de pessoa.
+
+      @param dado (string): o dado a ser processado
+      @return o vetor de strings contendo os diferentes campos de uma linha de
+              dados de pessoa
+      @throw caso o argumento seja inválido (invalid_argument)
+  */
+  vector<string> splitDado(string dado);
+
+  /**
+      Determina o índice do vector dadosPessoais em que está uma linha contendo
+      os dados de uma determinada pessoa.
+
+      @param idPessoa (string): o id da pessoa a ser localizada
+      @return o índice da string contida no dadosPessoais caso ela exista, -1
+              caso contrário
+  */
+  int getIndicePessoa(string idPessoa);
+
+  /**
+      Gera uma linha formatada contendo os dados relativos a uma pessoa.
+
+      @param idPessoa (string): o id da pessoa
+      @param idFuncional (string): o idFuncional da pessoa
+      @param estadoFuncional (string): o estado funcional da pessoa
+      @param nome (string): o nome da pessoa
+      @param profissao (string): a profissão da pessoa
+      @param endereco (string): o endereco da pessoa
+      @param funcao (string): a funcao da pessoa
+      @param cargo (string): o cargo da pessoa
+      @param faixaSalarial (string): a faixa salarial da pessoa
+      @param gratificacao (string): a gratificação salarial da pessoa
+      @return a string formatada contendo os dados da pessoa
+  */
+  string gerarLinha(string idPessoa, string idFuncional, string estadoFuncional,
+                    string nome, string profissao, string endereco,
+                    string funcao, string cargo, string faixaSalarial,
+                    string gratificacao);
+
+  string lerDadosPessoas(string valorChave, Campos chave);
+
+  string dadosCP();
+
+  bool atualizarDadosPessoas(string valChave, Campos chave, string novoValor, Campos campo);
+
+  bool inserir(string idPessoal,string idFuncional,string estadoFuncional, string  nome, string  profissão, string  endereço, string  função, string  cargo, string  faixaSalarial, string  gratificacao););
+
+private:
---
+++ aula11/src/lab10.cpp
@@ -0,0 +1,119 @@
+/**
+PCS2478 - Tópicos de Programação
+lab7.cpp
+
+@author 8586861 - Luiz Eduardo Sol (luizedusol@gmail.com)
+@author 7576829 - Augusto Ruy Machado (augustormachado@gmail.com)
+@version 1.0 2017-10-11
+*/
+
+#include "Empresa.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void sistema() {
+	string nomeEmpresa;
+	cout << "Forneca o nome da empresa:" << endl;
+	getline(cin, nomeEmpresa);
+	string tamanho;
+	cout << "Forneca o numero maximo de funcionarios da empresa " << nomeEmpresa
+		<< ":" << endl;
+
+	getline(cin, tamanho);
+
+	Empresa * aEmpresa;
+	string filtro = "";
+	string valorChave = "";
+	Campos chave;
+	try {
+		aEmpresa = new Empresa(nomeEmpresa, atoi(tamanho.c_str()));
+	}
+	catch (logic_error& ex) {
+		cout << "ERRO ao criar a empresa: " << ex.what() << endl;
+		cout << "Saindo..." << endl;
+		exit(1);
+	}
+	string option = "";
+
+	while (true) {
+		cout << endl << "-------------------------" << endl;
+		cout << aEmpresa->getNome() << endl;
+		cout << "-------------------------" << endl << endl;
+
+		cout << "[P] Mostrar Dados Pessoais" << endl;
+		cout << "[F] Mostrar Funcionarios Contratados" << endl;
+		cout << "[C] Contratar Funcionario" << endl;
+		cout << "[D] Demitir Funcionario" << endl;
+		cout << "[I] Inserir Pessoa" << endl;
+		getline(cin, option);
+		cout << "-------------------------" << endl << endl;
+
+		switch (option.at(0)) {
+		case 'P':
+		case 'p':
+			aEmpresa->cadastroPessoas.lerDadosTodasPessoas();
+			break;
+		case 'F':
+		case 'f':
+			aEmpresa->cadastroPessoas.lerDadosPessoas("1",C_EFUNC);
+			break;
+
+		case 'C':
+		case 'c':
+			string idPessoal;
+			cout << "Digite o idPessoal a ser contratado:" << endl;
+			getline(cin, idPessoal);
+			string idFuncional;
+			cout << "Digite o idFuncional do contratado:" << endl;
+			aEmpresa->contratarFuncCadastrado(idPessoal, idFuncional);
+			break;
+
+		case 'D':
+		case 'd':
+			string idFuncional;
+			cout << "Digite o idFuncional a ser demitido:" << endl;
+			getline(cin, idFuncional);
+			aEmpresa->demitirFuncionario(idFuncional);
+			break;
+
+		case 'I':
+		case 'i':
+			string idPessoal, nome, profissão, endereco, função, cargo, faixaSalarial;
+			cout << "Digite o idPessoal:";
+			getline(cin, idPessoal);
+			cout <<endl<< "Digite o nome:";
+			getline(cin, nome);
+			cout << endl << "Digite a profissao:";
+			getline(cin, profissão);
+			cout << endl << "Digite o endereco:";
+			getline(cin, endereco);
+			cout << endl << "Digite a funcao:";
+			getline(cin, função);
+			cout << endl << "Digite o cargo:";
+			getline(cin, cargo);
+			cout << endl << "Digite a faixa salarial:";
+			getline(cin, faixaSalarial);
+			cout << endl;
+
+			aEmpresa->inserirPessoaCadastro(idPessoal, nome, profissão, endereco, função, cargo, faixaSalarial);
+			break;
+		}
+	}
+}
+/*
+void teste(); {
+	AcessoDados * DADOS;
+	DADOS = new AcessoDados();
+	DADOS->atualizar(CAD_PESSOAS, "0002", C_IDPESSOA, "3", C_EFUNC);
+	DADOS->inserir(TAB_SALARIAL, "10|666.00|");
+	return 0; 
+}
+*/
+
+int main(int argc, char * argv[]) {
+	sistema();
+
+	return 0;
+}
