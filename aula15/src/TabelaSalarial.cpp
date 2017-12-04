61eddf522abc926871acab74a9fab54ed125d0cc
Luiz Sol <luizedusol@gmail.com>
2017-12-03 22:29:36 -0200

    Altera as datas dos arquivos

--- aula15/src/TabelaSalarial.cpp
+++ aula15/src/TabelaSalarial.cpp
@@ -4,7 +4,7 @@
 
     @author 8586861 - Luiz Eduardo Sol (luizedusol@gmail.com)
     @author 7576829 - Augusto Ruy Machado (augustormachado@gmail.com)
-    @version 15.0 2017-11-29
+    @version 15.0 2017-12-04
 */
 
 #include "TabelaSalarial.h"
